495148bb91a3b369399d1228bc2a3c420d653c06
RavenGard <rgardner1@ggc.edu>
2022-03-30 17:34:41 -0400

    First commit by repo admin Raven Gardner

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+/** Class Name: Student
+ * @author: Raven Gardner
+ * @version: 1.0
+ * Created: March 30, 2022
+ *
+ * Purpose: to hold student information
+ *
+ * purpose for group Git assignment: "create a java project with one empty class."
+ */
+public class Student {
+
+}
