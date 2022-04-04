2f5b4f784de01882fb7a4f55c0d1903c45e563f3
RavenGard <rgardner1@ggc.edu>
2022-04-04 17:36:11 -0400

    Commit by Raven; implemented start feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,8 +9,26 @@ public class Main {
         Game game = new Game();
         String input;
         String output;
+        String gameStart;
+        Scanner begin = new Scanner(System.in);
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("--------------------------");
+        System.out.println("Type 'start' to start game");
+        System.out.println("--------------------------");
+        gameStart = begin.next();
 
+        while (begin.hasNextLine()) {
+            if (gameStart.equalsIgnoreCase("start")) {
+                break;
+            } else {
+                System.out.println("invalid command\n");
+                System.out.println("--------------------------");
+                System.out.println("Type 'start' to start game");
+                System.out.println("--------------------------");
+                gameStart = begin.next();
+            }
+        }
+        game.showIntro();
         do {
             System.out.println("->");
             input = in.readLine();
@@ -17,7 +36,7 @@ public class Main {
 
             System.out.println(output);
 
-        } while (!input.equalsIgnoreCase("Q"));
+        } while (!input.equalsIgnoreCase("exit"));
 
     }
 
