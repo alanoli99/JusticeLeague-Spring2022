28d877b637d4d411869ac988520215033b346807
Dominick Kilbert <dkilbert@ggc.edu>
2022-04-06 16:15:41 -0400

    Added EquipArtifacts.java by Dominick Kilbert

---
+++ src/EquipArtifacts.java
@@ -0,0 +1,15 @@
+public class EquipArtifacts extends Artifacts{
+    private int ArtifactDamage;
+
+    public EquipArtifacts(int artifactDamage) {
+        ArtifactDamage = artifactDamage;
+    }
+
+    public int getArtifactDamage() {
+        return ArtifactDamage;
+    }
+
+    public void setArtifactDamage(int artifactDamage) {
+        ArtifactDamage = artifactDamage;
+    }
+}
