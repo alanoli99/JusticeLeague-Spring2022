79fa6cffe1b68b08bb72673cc149da837a74e035
Dominick Kilbert <dkilbert@ggc.edu>
2022-04-13 13:19:36 -0400

    Added Monsters.java, Monsters.txt, and EquipArtifacts.java by Dominick Kilbert

---
+++ src/EquipArtifacts.java
@@ -0,0 +1,19 @@
+/**
+ * @author : Dominick Kilbert
+ */
+
+public class EquipArtifacts{
+    private int ArtifactDamage;
+
+    public EquipArtifacts(int artifactDamage) {
+        ArtifactDamage = artifactDamage;
+    }
+
+    public int getArtifactDamage() {
+        return ArtifactDamage;
+    }
+
+    public void setArtifactDamage(int artifactDamage) {
+        ArtifactDamage = artifactDamage;
+    }
+}
