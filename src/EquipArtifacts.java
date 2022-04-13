9b58d30d10a70333e6c77b3e13d5b5ee0551f4a4
Dominick Kilbert <dkilbert@ggc.edu>
2022-04-13 13:22:03 -0400

    Updated EquipArtifacts.java by Dominick Kilbert

--- src/EquipArtifacts.java
+++ src/EquipArtifacts.java
@@ -2,12 +2,13 @@
  * @author : Dominick Kilbert
  */
 
-public class EquipArtifacts{
+public class EquipArtifacts extends Artifacts{
     private int ArtifactDamage;
 
-    public EquipArtifacts(int artifactDamage) {
-        ArtifactDamage = artifactDamage;
+    public EquipArtifacts(int artiID, String artiName, String artiDescription, String artiType, String artiUsage, int consumeHealth, int equipHealth) {
+        super(artiID, artiName, artiDescription, artiType, artiUsage, consumeHealth, equipHealth);
     }
+
 
     public int getArtifactDamage() {
         return ArtifactDamage;
