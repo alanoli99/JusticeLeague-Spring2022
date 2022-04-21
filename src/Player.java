eb4ec0fdb7b7d8427140309d36cfcd07a2b9e86d
alanoli99 <alanoli99@gmail.com>
2022-04-21 11:29:20 -0400

    item drops now from the drop item method. fixing it so if there is nothing in inventory it displays that

--- src/Player.java
+++ src/Player.java
@@ -7,16 +7,21 @@ import java.util.Map;
  *
  * @author: Raven Gardner
  * created attributes, getter/setter, and hashmap info
- * Methods: consume, equip
+ * Methods: consume
+ *
+ *
  */
 
 public class Player extends Rooms {
     protected static Rooms location;
     protected static int playerHealth;
     protected static int playerAttkDamage;
-    private static HashMap<Artifacts, String> equippedItems = new HashMap<>();
+    private static HashMap<Artifacts, String> equippedItems = Text.getHashMapForArti(); // will change later to new HashMap<>() -- Raven
+
     private static HashMap<Artifacts, String> inventoryMap = new HashMap<>();
+
     private static HashMap<Artifacts, Integer> playerInfo = new HashMap<>(); // key = Artifacts info, value = health -- Raven
+
     public static ArrayList<Artifacts> inventory = new ArrayList<>();
 
     public static ArrayList<Artifacts> getInventory() {
@@ -45,9 +50,9 @@ public class Player extends Rooms {
         return playerHealth;
     }
 
-    public static void setPlayerHealth(int health) {
+    public static int setPlayerHealth(int health) {
         Player.playerHealth += health;
-        // return health;
+        return health;
     }
 
     public static int getPlayerAttkDamage() {
@@ -66,6 +71,10 @@ public class Player extends Rooms {
         return equippedItems;
     }
 
+    public static void setEquippedItems(HashMap<Artifacts, String> equippedItems) {
+        Player.equippedItems = equippedItems;
+    }
+
     public static HashMap<Artifacts, Integer> getPlayerInfo() {
         return playerInfo;
     }
@@ -80,13 +89,13 @@ public class Player extends Rooms {
      * ID: IF7; players can consume certain items
      */
     public static String consume(String obname) {
-        String beenConsumed = "";
+        String beenEquipped = "";
         Artifacts inInventory;
         int healthPoints;
 
-        //for (Artifacts artifacts : getInventory()) {
-            //getPlayerInventoryMap().put(artifacts, artifacts.getArtiName());
-        //}
+        for(Artifacts artifacts : getInventory()){
+            getPlayerInventoryMap().put(artifacts, artifacts.getArtiName());
+        }
 
         obname = obname.trim().replaceAll("\\s{2,}", " "); // gets rid of the space in the beginning of the item name -- Raven
 
@@ -100,59 +109,15 @@ public class Player extends Rooms {
             //System.out.println("obj name:"+obname);
             if (inInventory.getArtiName().equalsIgnoreCase(obname)) {
                 if (inInventory.getConsumeHealth() == 0) {
-                    beenConsumed = "\n" + obname + " cannot be consumed!";
+                    beenEquipped = "\n" + obname + " cannot be consumed!";
+                } else if(getPlayerHealth() >= 50 && inInventory.getConsumeHealth() >= 0){
+                    beenEquipped = "\n" + "Your health is full this cannot be consumed";
                 } else {
+                    // right here get the attack damage from the item hashmap after I fix the item file, item class, and room class with hashmap
                     healthPoints = inInventory.getConsumeHealth();
                     setPlayerHealth(healthPoints);
                     playerInfo.clear();
-                    beenConsumed = "\n" + obname + " has been consumed! " + obname + " has changed your health by " + healthPoints + " points!\n" +
-                            "You now have " + getPlayerHealth() + " health points.";
-                    //will drop item out of inventory
-                    //System.out.println("inventory before: " + getInventory());
-                    getInventory().remove(collected.getKey());
-                    getPlayerInventoryMap().remove(inInventory);
-                    //System.out.println("inventory after: " + getInventory());
-                }
-                playerInfo.put(inInventory, getPlayerHealth());
-                break;
-            }
-        }
-        if (beenConsumed.isEmpty()) {
-            beenConsumed = "\nThere is/are no " + obname + " in inventory \n";
-        }
-        return beenConsumed;
-    }
-
-    /**
-     * @author: Raven Gardner
-     * players can equip items
-     */
-    public static String equip(String obname) {
-        String beenEquipped = "";
-        Artifacts inInventory;
-        int healthPoints;
-
-        //for (Artifacts artifacts : getInventory()) {
-            //getPlayerInventoryMap().put(artifacts, artifacts.getArtiName());
-        //}
-
-        obname = obname.trim().replaceAll("\\s{2,}", " "); // gets rid of the space in the beginning of the item name -- Raven
-
-        for (Map.Entry<Artifacts, String> collected : getPlayerInventoryMap().entrySet()) {
-
-            inInventory = collected.getKey();
-            //System.out.println("in inventory: " + inInventory);
-
-            //System.out.println("in inventory:" + inInventory.getArtiName());
-            // System.out.println("obj name:"+obname);
-            if (inInventory.getArtiName().equalsIgnoreCase(obname)) {
-                if (inInventory.getEquipHealth() == 0) {
-                    beenEquipped = "\n" + obname + " cannot be equipped!";
-                } else {
-                    healthPoints = inInventory.getEquipHealth();
-                    setPlayerHealth(healthPoints);
-                    playerInfo.clear();
-                    beenEquipped = "\n" + obname + " has been equipped! " + obname + " has changed your health by " + healthPoints + " points!\n" +
+                    beenEquipped = "\n" + obname + " has been consumed! " + obname + " has changed your health by " + healthPoints + " points!\n" +
                             "You now have " + getPlayerHealth() + " health points.";
                     //will drop item out of inventory into room
                     //System.out.println("inventory before: " + getInventory());
@@ -165,73 +130,34 @@ public class Player extends Rooms {
             }
         }
         if (beenEquipped.isEmpty()) {
-            beenEquipped = "\nThere is/are no " + obname + " in inventory \n";
+            beenEquipped = "\nThere are/is no " + obname + " in inventory \n";
         }
         return beenEquipped;
     }
 
-    /**
-     * @author: Raven Gardner
-     * players can unequip items
-     */
-    public static String unequip(String obname) {
-        String beenEquipped = "";
-        Artifacts inEquipMap;
-        int healthPoints;
-
-        //for (Artifacts artifacts : getInventory()) {
-        //getPlayerInventoryMap().put(artifacts, artifacts.getArtiName());
-        //}
-
-        obname = obname.trim().replaceAll("\\s{2,}", " "); // gets rid of the space in the beginning of the item name -- Raven
-
-        for (Map.Entry<Artifacts, String> equipped : getEquippedItems().entrySet()) {
-
-            inEquipMap = equipped.getKey();
-            //System.out.println("in inventory: " + inInventory);
-
-            //System.out.println("in inventory:" + inInventory.getArtiName());
-            // System.out.println("obj name:"+obname);
-            if (inEquipMap.getArtiName().equalsIgnoreCase(obname)) {
-                healthPoints = inEquipMap.getEquipHealth();
-                setPlayerHealth(-healthPoints);
-                playerInfo.clear();
-                beenEquipped = "\n" + obname + " has been unequipped!" +
-                        "\nYou now have " + getPlayerHealth() + " health points.";
-
-                playerInfo.put(inEquipMap, getPlayerHealth());
-                break;
-            }
-        }
-        if (beenEquipped.isEmpty()) {
-            beenEquipped = "\n" + obname + " has not been equipped yet\n";
-        }
-        return beenEquipped;
-    }
-
-    public static void addToInventory(String noun) {
+        public static void addToInventory(String noun) {
         ArrayList<Artifacts> artifactsList = new ArrayList<>();
         Text.artiList(artifactsList);
         Artifacts artifacts;
         noun = noun.trim().replaceAll("\\s{2,}", " ");
         artifacts = Artifacts.getItemObject(Game.getRooms().getItemID(), artifactsList);
-        if (artifacts != null && artifacts.getArtiName().equalsIgnoreCase(noun)) {
-            if (inventory.size() >= 5) {
+        if (artifacts != null && artifacts.getArtiName().equalsIgnoreCase(noun)){
+            if (inventory.size() >= 5){
                 System.out.println("Inventory is full! drop item.");
                 return;
             }
             inventory.add(artifacts);
             System.out.println(noun + " has been added to inventory");
-           // System.out.println(inventory.size());
-            getPlayerInventoryMap().put(artifacts,noun); //Raven -- created to update map for inventory
-
-        } else if (artifacts != null && !artifacts.getArtiName().equalsIgnoreCase(noun)) {
-            if (inventory.size() > 5) {
+            System.out.println(inventory.size());
+        }
+        else if(artifacts != null && !artifacts.getArtiName().equalsIgnoreCase(noun) ){
+            if (inventory.size() > 5){
                 System.out.println("Inventory is full! drop item.");
                 return;
             }
             System.out.println(noun + " is not in this room");
-        } else {
+        }
+        else{
             System.out.println("There's no item in this room");
         }
 
@@ -246,20 +172,20 @@ public class Player extends Rooms {
         noun = noun.trim().replaceAll("\\s{2,}", " ");
         artifacts = Artifacts.getItemObject(Player.getLocation().getItemID(), artifactsList);
 
-        if (artifacts != null && artifacts.getArtiName().equalsIgnoreCase(noun)) {
-            if (inventory.size() == 0) {
+        if (artifacts != null && artifacts.getArtiName().equalsIgnoreCase(noun)){
+            if (inventory.size() == 0){
                 System.out.println("Inventory is empty! no item to drop!");
                 return;
             }
             inventory.remove(artifacts);
             System.out.println(noun + " has been dropped from inventory");
             System.out.println(inventory.size());
-        } else {
+        }
+        else{
             System.out.println("There's no item in your inventory!");
         }
 
     }
-
     @Override
     public String toString() {
         return "\nPlayer info\n\n" +
